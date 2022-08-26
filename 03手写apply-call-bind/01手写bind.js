821e47f273c928d480366e3b1dbdb5bf456fc87d
梦醒如初 <675232630@qq.com>
2022-08-26 23:05:25 +0800

    手写bind

--- 03手写apply-call-bind/01手写bind.js
+++ 03手写apply-call-bind/01手写bind.js
@@ -1,3 +1,24 @@
 /**
  * bind() 最简单的用法是创建一个函数，不论怎么调用，这个函数都有同样的 this 值
  */
+Function.prototype.myBind = function (thisArg, ...argArray) {
+  //1.绑定this
+  let fn = this;
+  //边界判断:1.对thisArg转为对象类型(防止它传入的是非对象类型)2.判断null、undefined(自动替换为指向全局对象:window)
+  thisArg = thisArg ? Object(thisArg) : window;
+  return function proxyFn(...args) {
+    //2.调用需要被执行的函数
+    thisArg.fn = fn;
+    let finalArgs = [...argArray, ...args];
+    let result = thisArg.fn(...finalArgs);
+    //3.删除属性
+    delete thisArg.fn();
+    //4.返回结果
+    return result;
+  };
+};
+function add(num1, num2) {
+  return num1 + num2;
+}
+console.log("系统调用:" + add.bind({})(123, 456));
+console.log("编写call函数调用:" + add.myBind({})(123, 456));
--- 03手写apply-call-bind/02手写call.js
+++ 03手写apply-call-bind/02手写call.js
@@ -5,7 +5,7 @@
  * call() 方法使用一个指定的 this 值和单独给出的一个或多个参数来调用一个函数
  */
 Function.prototype.myCall = function (thisArg, ...res) {
-  //1.获取被执行的函数
+  //1.绑定this
   let fn = this;
   //边界判断:1.对thisArg转为对象类型(防止它传入的是非对象类型)2.判断null、undefined(自动替换为指向全局对象:window)
   thisArg = thisArg ? Object(thisArg) : window;
